3d842bd7dee0e2d83880a422671c78f22866826d
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2022-11-16 15:34:03 -0800

    added imports, DOM elements and events

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 /* Imports */
+import { fetchPosts, getUser, logout } from './fetch.utils.js';
 
 /* Get DOM Elements */
-
+const bulletin = document.getElementById('bulletin-board');
+const authButton = document.getElementById('auth-button');
+const createButton = document.getElementById('create');
 /* State */
 
 /* Events */
+window.addEventListener('load', async () => {
+    const user = await getUser();
 
+    createButton.addEventListener('click', () => {
+        location.replace('/create');
+    });
+});
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
--- fetch.utils.js
+++ fetch.utils.js
@@ -2,3 +2,13 @@ const SUPABASE_URL = 'https://zgixhmlshitskkemwyaf.supabase.co';
 const SUPABASE_KEY =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnaXhobWxzaGl0c2trZW13eWFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDgwODYsImV4cCI6MTk4MzY4NDA4Nn0.nMjJ-vp1PSZuD_oT9AQGKADmPu3OCZp9Uf4n2XbaBjQ';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function getUser() {
+    return client.auth.session() && client.auth.session().user;
+}
+
+export async function fetchPosts() {
+    const response = await client.from('posts').select('*');
+
+    return response.data;
+}
