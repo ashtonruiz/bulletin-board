bdc8ea24baf377882707425e8e89b1369b1c4707
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2022-11-16 16:09:00 -0800

    completed events in app.js

--- app.js
+++ app.js
@@ -12,9 +12,25 @@ const createButton = document.getElementById('create');
 window.addEventListener('load', async () => {
     const user = await getUser();
 
+    if (user) {
+        authButton.addEventListener('click', logout);
+        authButton.textContent = 'Logout';
+    } else {
+        authButton.addEventListener('click', () => {
+            location.replace('/auth');
+        });
+        authButton.textContent = 'Login';
+    }
+
     createButton.addEventListener('click', () => {
         location.replace('/create');
     });
+
+    const posts = await getPosts();
+    for (let post of posts) {
+        const postDiv = renderPostIt(post);
+        bulletin.append(postDiv);
+    }
 });
 /* Display Functions */
 
